3b2296040e9ac2b3a71403201a8d5cd75be4a64c
aubertra <aubertra@aubertra.42.fr>
2025-08-07 18:06:41 +0200

    hopefully cna be finished tomorrow, all containers up, pb is port 443

--- srcs/requirements/wordpress/conf/wordpress.sh
+++ srcs/requirements/wordpress/conf/wordpress.sh
@@ -7,14 +7,18 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/bin/wp
 
-#give enough memory to php to download WP
-export WP_CLI_PHP_ARGS='-d memory_limit=256M'
-
 #Getting ready for WP installation
 mkdir -p /var/www/wordpress
 cd /var/www/wordpress
 chmod -R 755 /var/www/wordpress
 chown -R www-data:www-data /var/www/wordpress
+
+# Wait for MariaDB to become available
+echo "⏳ Waiting for MariaDB to be ready..."
+until mysqladmin ping -hmariadb --silent; do
+    sleep 1
+done
+echo "✅ MariaDB is ready!"
 
 #WP installation and set up
 wp core download --allow-root
@@ -43,8 +47,8 @@ wp user create\
 
 #Config PHP
 #make sure php is installed on the VM
-sed -i 's|^listen = .*|listen = 9000|' /etc/php82/php-fpm.d/www.conf
+sed -i 's|^listen = .*|listen = 9000|' /etc/php/7.4/fpm/pool.d/www.conf
 
 mkdir -p /run/php
 #-F means it starts in the foreground 
-/usr/sbin/php-fpm82 -F
+/usr/sbin/php-fpm7.4 -F
